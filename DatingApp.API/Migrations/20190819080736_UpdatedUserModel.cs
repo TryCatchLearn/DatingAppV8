59b61d1cfbc2bca2ab510a93be037c49eec7ec7e
trycatchlearn <trycatchlearn@outlook.com>
2019-08-19 15:44:43 +0700

    End of section 9

---
+++ DatingApp.API/Migrations/20190819080736_UpdatedUserModel.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace DatingApp.API.Migrations
+{
+    public partial class UpdatedUserModel : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Introduction",
+                table: "Users",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Introduction",
+                table: "Users");
+        }
+    }
+}
--- DatingApp.API/Models/User.cs
+++ DatingApp.API/Models/User.cs
@@ -14,6 +14,7 @@ namespace DatingApp.API.Models
         public string KnownAs { get; set; }
         public DateTime Created { get; set; }
         public DateTime LastActive { get; set; }
+        public string Introduction { get; set; }
         public string LookingFor { get; set; }
         public string Interests { get; set; }
         public string City { get; set; }
