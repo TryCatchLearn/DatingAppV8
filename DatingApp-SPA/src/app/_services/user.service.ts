66a8992a062582271f348a3c9cfe989fdc0904c7
trycatchlearn <trycatchlearn@outlook.com>
2019-08-20 16:32:28 +0700

    End of section 14

--- DatingApp-SPA/src/app/_services/user.service.ts
+++ DatingApp-SPA/src/app/_services/user.service.ts
@@ -1,8 +1,10 @@
 import { Injectable } from '@angular/core';
 import { environment } from 'src/environments/environment';
-import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { User } from '../_models/user';
+import { PaginatedResult } from '../_models/pagination';
+import { map } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
@@ -12,8 +14,33 @@ export class UserService {
 
   constructor(private http: HttpClient) {}
 
-  getUsers(): Observable<User[]> {
-    return this.http.get<User[]>(this.baseUrl + 'users');
+  getUsers(page?, itemsPerPage?, userParams?): Observable<PaginatedResult<User[]>> {
+    const paginatedResult: PaginatedResult<User[]> = new PaginatedResult<User[]>();
+
+    let params = new HttpParams();
+
+    if (page != null && itemsPerPage != null) {
+     params = params.append('pageNumber', page);
+     params = params.append('pageSize', itemsPerPage);
+   }
+
+    if (userParams != null) {
+     params = params.append('minAge', userParams.minAge);
+     params = params.append('maxAge', userParams.maxAge);
+     params = params.append('gender', userParams.gender);
+     params = params.append('orderBy', userParams.orderBy);
+   }
+
+    return this.http.get<User[]>(this.baseUrl + 'users', { observe: 'response', params})
+     .pipe(
+       map(response => {
+         paginatedResult.result = response.body;
+         if (response.headers.get('Pagination') != null) {
+           paginatedResult.pagination = JSON.parse(response.headers.get('Pagination'));
+         }
+         return paginatedResult;
+       })
+     );
   }
 
   getUser(id): Observable<User> {
